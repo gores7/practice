4898ef4c92a1bd7c4e4ce9157d04657caeed5bd5
user <user@ubuntu.myguest.virtualbox.org>
2025-01-22 15:01:34 +0300

    Add connection to DB

---
+++ createConnection.php
@@ -0,0 +1,8 @@
+<?php
+
+$Hostname = 'localhost';
+$DatabaseName = 'practice';
+$DatabaseUser = 'postgres';
+$DatabasePassword = 'kuzko1234';
+$port = '5432';
+$connect = new PDO("pgsql:host=$Hostname;dbname=$DatabaseName;port=$port;", $DatabaseUser, $DatabasePassword);
